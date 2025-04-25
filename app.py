279310f76f51393b77cfc395bdb84cad0c3dd076
石塚 巧 <ishizukatakumi@zukanoMacBook-Air.local>
2025-04-25 14:43:49 +0900

    1st commit

---
+++ app.py
@@ -0,0 +1,109 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import datetime as datetime
+
+import plotly.graph_objects as go
+
+import sklearn.linear_model
+import sklearn.model_selection
+from PIL import Image
+import yfinance as yf
+
+st.title("AIで株価予想")
+st.write("AIを使って株価を予想するアプリです。")
+
+image = Image.open('stock_predict.png')
+st.image(image,use_column_width=True)
+
+st.write("あくまでAIによる予測です（参考値）。こちらのアプリによる損害や過失については一切責任を負いません。")
+
+st.header("株価銘柄のティッカーシンボルを入力してください")
+stock_name=st.text_input("例:AAPL,FB,SFTBY ","AAPL")
+
+stock_name=stock_name.upper()
+
+link='https://search.sbisec.co.jp/v2/popwin/info/stock/pop6040_usequity_list.html'
+st.markdown(link)
+st.write("ティッカーシンボルは、上部LINKで確認できます。")
+
+try:
+    df_stock=yf.download(stock_name, start="2011-01-05" )
+    df_stock.columns=df_stock.columns.droplevel(1)
+    st.header(stock_name+'2022年1月5日から現在までの株価（USD）')
+    st.write(df_stock)
+
+    st.header(stock_name+"終わり値と14日間平気（USD）")
+    df_stock['SMA']=df_stock['Close'].rolling(window=14).mean()
+    df_stock2=df_stock[['Close','SMA']]
+    st.line_chart(df_stock2)
+
+    st.header(stock_name+"値動き（USD）")
+    df_stock['change']=(((df_stock['Close']-df_stock['Open']))/(df_stock['Open'])*100)
+    st.line_chart(df_stock['change'].tail(100))
+# st.line_chart(df_stock['change'].tail(100))
+
+    fig=go.Figure(
+      data=[go.Candlestick(
+          x=df_stock.index,
+           open=df_stock['Open'],
+           high=df_stock['High'],
+           low=df_stock['Low'],
+           close=df_stock['Close'],
+           increasing_line_color='green',
+           decreasing_line_color='red',)]
+)
+
+    st.header(stock_name+"ローソク足")
+    st.plotly_chart(fig, use_column_width=True)
+
+    df_stock['label']=df_stock['Close'].shift(-30)
+
+    st.header(stock_name+'1ヶ月後を予測（USD）')
+    def stock_predict():
+        X=np.array(df_stock.drop(['label','SMA'],axis=1))
+        X=sklearn.preprocessing.scale(X)
+        predict_data=X[-30:]
+        X=X[:-30]
+        y=np.array(df_stock['label'])
+        y=y[:-30]
+
+        X_train,X_test,y_train,y_test=sklearn.model_selection.train_test_split(
+            X,y,test_size=0.2)
+        model=sklearn.linear_model.LinearRegression()
+        model.fit(X_train,y_train)
+
+        accuracy=model.score(X_test,y_test)
+        st.write(f'正解率は{round(accuracy)*100,1}%です')
+
+        if accuracy>0.75:
+            st.write('信頼度:高')
+        elif accuracy>0.5:
+            st.write('信頼度:中')
+        else:
+            st.write('信頼度:低')
+        st.write('色が変わっている部分が予測値です')
+
+        predicted_data=model.predict(predict_data)
+        df_stock['Predict']=np.nan
+        last_date=df_stock.iloc[-1].name
+        one_day=86400
+        next_unix=last_date.timestamp()+one_day
+
+        for data in predicted_data:
+            next_date=datetime.datetime.fromtimestamp(next_unix)
+            next_unix+=one_day
+            df_stock.loc[next_date]=np.append([np.nan]*(len(df_stock.columns)-1),data)
+
+        df_stock['Close'].plot(figsize=(15,6),color='green')
+        df_stock['Predict'].plot(figsize=(15,6),color='orange')
+
+        df_stock3=df_stock[['Close','Predict']]
+        st.line_chart(df_stock3)
+
+    if st.button('予測する'):
+      stock_predict()
+except:
+    st.error('ティッカーシンボルが正しくありません。')
+st.write('Copyright © 2021, Streamlit, Inc. All rights reserved.')
+
